702ac851bbdcdee975f188c78f0f96ac9f6003a5
Gary Su <garysu@Garys-Laptop.local>
2023-07-12 11:30:36 -0400

    neurel net class

--- app.py
+++ app.py
@@ -5,3 +5,17 @@ import streamlit as st
 from streamlit_drawable_canvas import st_canvas
 import numpy as np
 # from scipy.ndimage.interpolation import zoom
+
+class NN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Conv2d(1, 32, (3, 3)),
+            nn.ReLU(),
+            nn.Conv2d(32, 48, (3, 3)),
+            nn.ReLU(),
+            nn.Conv2d(48, 64, (3, 3)),
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(64 * (28 - 6) * (28 - 6), 10)
+        )
