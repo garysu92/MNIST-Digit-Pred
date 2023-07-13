87d3fa34003a969bda48ac5e561a22173a2d60aa
Gary Su <garysu929292@gmail.com>
2023-07-12 23:46:11 -0400

    get data

--- train.py
+++ train.py
@@ -4,3 +4,5 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 
+train_data = datasets.MNIST(root = "data", download = True, train = True, transform = ToTensor())
+train_data_set = DataLoader(train_data, 16)
