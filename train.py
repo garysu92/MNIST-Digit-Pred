070701e8d2a405dfccb52cfb26271682e426eb0e
Gary Su <garysu929292@gmail.com>
2023-07-12 23:47:02 -0400

    paste nn class

--- train.py
+++ train.py
@@ -6,3 +6,22 @@ from torchvision.transforms import ToTensor
 
 train_data = datasets.MNIST(root = "data", download = True, train = True, transform = ToTensor())
 train_data_set = DataLoader(train_data, 16)
+
+class NN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Conv2d(1, 32, (3, 3)),
+            nn.ReLU(),
+            nn.Conv2d(32, 48, (3, 3)),
+            nn.ReLU(),
+            nn.Conv2d(48, 64, (3, 3)),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, (3, 3)),
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(64 * (28 - 8) * (28 - 8), 10)
+        )
+    
+    def forward(self, x):
+        return self.model(x)
